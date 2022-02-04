21b3f355b62817465a80894b563fec13619fb622
erin-day-2 <daye4@mail.uc.edu>
2022-02-04 14:22:19 -0500

    notes

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.sql.SQLOutput;
+
+public class Main
+{
+
+    // single line -- Java Variable Types: String, int, double, boolean
+
+    /* multiline
+
+     */
+    public static void main(String[] args)
+    {
+        int age = 25;
+        int val1 = 4;
+        int val2 = 6;
+        int sum = 0;
+        int product = 0;
+        int divResult = 0;
+        int subtractResult = 0;
+
+        boolean isRaining = true;
+        double salary = 500000.0;
+        String fName = "Tom";
+
+        System.out.println(fName + " salary is " + salary );
+
+        sum = val1 + val2;
+        System.out.println("the sum of " + val1 + " and " + val2 + " is " + sum);
+
+    }
+}
+
